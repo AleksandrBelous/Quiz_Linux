60e92d4ac2905af4ddfdd883837083c00f4ac048
Aleksandr Belous <ab21082001@yandex.ru>
2023-03-18 19:23:36 +0300

    added examples

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from rich import print as rprint
 from rich.console import Console
+from rich.table import Table
 
 nums_list = [1, 2, 3, 4]
 rprint(nums_list)
@@ -22,3 +23,31 @@ def merge_dict(dict_one, dict_two):
 
 
 merge_dict({'id': 1}, {'name': 'Ashutosh'})
+console.print("Hello", "World!", style="bold red")
+console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
+
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold magenta")
+table.add_column("Date", style="dim", width=12)
+table.add_column("Title")
+table.add_column("Production Budget", justify="right")
+table.add_column("Box Office", justify="right")
+table.add_row(
+        "Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$275,000,000", "$375,126,118"
+)
+table.add_row(
+        "May 25, 2018",
+        "[red]Solo[/red]: A Star Wars Story",
+        "$275,000,000",
+        "$393,151,347",
+)
+table.add_row(
+        "Dec 15, 2017",
+        "Star Wars Ep. VIII: The Last Jedi",
+        "$262,000,000",
+        "[bold]$1,332,539,889[/bold]",
+)
+
+console.print(table)
