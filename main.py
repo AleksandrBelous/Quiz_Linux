c2852a5a468b10da9ba2f7d74aeaa92abd625a1f
Aleksandr Belous <ab21082001@yandex.ru>
2023-03-20 22:19:26 +0300

    Some changes

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i
 console = Console()
 
 table = Table(show_header=True, header_style="bold magenta")
-table.add_column("Date", style="dim", width=12)
-table.add_column("Title")
-table.add_column("Production Budget", justify="right")
+table.add_column("Дата", style="dim", width=12)
+table.add_column("Название")
+table.add_column("Бюджет", justify="right")
 table.add_column("Box Office", justify="right")
 table.add_row(
         "Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$275,000,000", "$375,126,118"
